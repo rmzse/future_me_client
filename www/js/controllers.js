e8938305d8140a426066755550a85b4da6d4e83f
aniansson <ebba@aniansson.se>
2017-02-15 12:15:52 +0100

    Create view, not showing

---
+++ www/js/controllers.js
@@ -0,0 +1,10 @@
+angular.module('future_me.controllers', [])
+
+  .controller('AppCtrl', function ($scope) {
+
+
+  })
+  .controller('DescriptionController', function() {
+    console.log('description controller');
+
+  });
