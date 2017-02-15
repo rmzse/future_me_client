11ecf7653de3f8031336ac39971415daeafc49a7
Philip <Philip.zdb@gmail.com>
2017-02-15 11:17:47 +0100

    Add changes to ionic tinder cards (now functional)

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('futureme', ['ionic'])
+angular.module('futureme', ['ionic', 'futureme.controllers', 'ionic.contrib.ui.tinderCards'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -30,7 +30,7 @@ angular.module('futureme', ['ionic'])
       .state('suggestions', {
         url: '/suggestions',
         templateUrl: 'templates/suggestions.html',
-        controller: 'suggestionsCtrl'
+        controller: 'cardsCtrl'
       });
 
     // if none of the above states are matched, use this as the fallback
