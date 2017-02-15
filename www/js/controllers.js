7f5c61e9dcffcb800ea9d8a6196d935940b7a0db
Philip <Philip.zdb@gmail.com>
2017-02-15 16:25:33 +0100

    Adds new bg image + changes in view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,5 +2,9 @@ angular.module('futureme.controllers', [])
 
   .controller('cardsCtrl', function ($scope, TDCardDelegate) {
 
+    $scope.cards = [
+      {title: "bagare", image: "../img/chef.png"},
+      {title: "sjuksköterska", image: "../img/nurse.png"}
+    ]
 
   });
