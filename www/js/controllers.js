217cf3650384fd29e7d314b57c6a74338f3bd9dc
Philip Zudemberg <philip.zdb@gmail.com>
2017-02-17 11:14:14 +0100

    Carousel (swipe right) (#9)
    * Rewrites the cardCtrl and adds readme
    
    * Replaces old tinder cards library with new one, carousel now functional one direction
    
    * Updates readme
    
    * Adds additional cards
    
    * Fixes bug, cards has to be removed from array with destroy function, otherwise it will not work properly
    
    * Fixes typo in a card
    
    * Removes console logs, and arrays not being used
    
    * Fixes typo
    
    * Adds so array length is being checked upon card destroy instead of card swipe
    
    * Replaces old occupation images with new high res, adds id to every occupation

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,42 @@
 angular.module('futureme.controllers', [])
 
-  .controller('cardsCtrl', function ($scope, TDCardDelegate) {
+  .controller('cardsCtrl', function ($scope, TDCardDelegate, $timeout) {
 
-    $scope.cards = [
-      {title: "bagare", image: "chef.png"},
-      {title: "sjuksköterska", image: "nurse.png"}
-    ]
+    var cardTypes = [{id: 1, title: "bagare", image: "Bagare.png"},
+      {id: 2, title: "sjuksköterska", image: "Sjuksköterska.png"},
+      {id: 3, title: "möbeldesigner", image: "Mobeldes.png"},
+      {id: 4, title: "läkare", image: "Sjuksköterska.png"}];
 
+    $scope.cards = {
+      master: Array.prototype.slice.call(cardTypes, 0),
+      active: Array.prototype.slice.call(cardTypes, 0)
+    };
+
+    $scope.cardDestroyed = function (index) {
+      $scope.cards.active.splice(index, 1);
+      if ($scope.cards.active.length === 0)
+        $scope.refreshCards();
+    };
+
+    $scope.addCard = function () {
+      var newCard = cardTypes[0];
+      $scope.cards.active.push(angular.extend({}, newCard));
+    };
+
+    $scope.refreshCards = function () {
+      $scope.cards.active = null;
+      $timeout(function () {
+        $scope.cards.active = Array.prototype.slice.call($scope.cards.master, 0);
+      });
+    };
+
+    $scope.cardSwipedLeft = function (index) {
+    };
+
+    $scope.cardSwipedRight = function (index) {
+    };
+  })
+
+  .controller('cardCtrl', function ($scope, TDCardDelegate) {
   });
+
