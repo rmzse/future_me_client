4b33405711911011ac7140c20093a8d1055f6bf3
aniansson <ebba@aniansson.se>
2017-02-18 14:44:18 +0100

    Add goback nav in lib screen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -73,6 +73,8 @@ angular.module('futureme.controllers', [])
     };
   })
 
-  .controller('libraryController', function ($scope) {
-
+  .controller('libraryController', function ($scope, $ionicHistory) {
+    $scope.myGoBack = function () {
+      $ionicHistory.goBack();
+    };
   });
